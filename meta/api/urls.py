8c3b729e91d0d103c80485f4c524d037445bce3b
Chengzhi Gu <cgu@pinterest.com>
2015-03-07 13:07:17 -0800

    fix bug

--- meta/api/urls.py
+++ meta/api/urls.py
@@ -5,5 +5,6 @@ from api import views
 urlpatterns = patterns(
     '',
     url(r'^image/$', views.image_handler, name='image'),
-    url(r'^recipe/$', views.recipe_handler, name='recipe')
+    url(r'^recipe/$', views.recipe_handler, name='recipe'),
+    url(r'^list/$', views.list_handler, name='list')
 )
